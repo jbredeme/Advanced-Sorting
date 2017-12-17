58c02fa5e64ce73aa6cb5664476099ab3a615c7b
jbredeme <jpb64@nau.edu>
2017-12-16 23:39:24 -0700

    C# implementation

---
+++ merge_sort/c#/MergeSortCS/MergeSortApp/MergeSortApp.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using MergeSortLib;
+
+namespace MergeSortApp {
+    /// <summary>
+    /// Performs merge sort (recursive implementation) on an integer array.
+    /// </summary>
+    class MergeSortApp {
+
+        static void Main(string[] args) {
+            Random rnd = new Random();
+            const int MAX_SIZE = 64;
+            Boolean isValid;                        //=> do loop flag
+            int size = 0;                           //=> size of array
+            int[] arr;                              //=> array
+            string range;                           //=> range input
+            string[] bounds;                        //=> split range input
+            int upperBound = 0, lowerBound = 0;     //=> range bounds
+            string message = "Welcome to Merge Sort in C#";
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(message);
+ 
+            do {                            //=> input entry for array size
+                Console.ForegroundColor = ConsoleColor.White;
+                isValid = false;
+                Console.WriteLine("\nPlease enter an array size that is greater than 0 and less than or equal to {0}:", MAX_SIZE);
+
+                try {                       //=> guard against non-numeric data
+                    size = Convert.ToInt32(Console.ReadLine());     
+
+                    try {                   //=> guard against invalid array size
+                        if (size <= 0) {
+                            throw new OverflowException();
+
+                        }
+
+                        try {
+                            if (size > MAX_SIZE) {
+                                throw new OverflowException();
+
+                            }
+
+                            isValid = true;
+
+                        } catch (OverflowException e) {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine(".. Invaild input, {0} is larger than {1}.\n", size, MAX_SIZE);
+
+                        }
+                       
+                    }
+                    catch (OverflowException e) {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine(".. Invaild input, you cannot have an array of size {0}.\n", size);
+
+                    }
+
+                } catch (FormatException e) {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(".. Invaild input, input must be of TYPE integer.\n");
+
+                }
+
+            } while (isValid != true);
+
+            do {                            //=> input entry for element range
+                Console.ForegroundColor = ConsoleColor.White;
+                isValid = false;
+
+                Console.WriteLine("\nPlease enter a range (integer values only) lower bound <space> upper bound:");
+                range = Console.ReadLine();
+                bounds = range.Split(' ', '\t');
+
+                try {                       //=> check for correct range format (two values)
+                    if (bounds.Length != 2) {
+                        throw new ArgumentOutOfRangeException();
+
+                    }
+
+                    try {                   //=> guard against non-numeric data
+                        lowerBound = Convert.ToInt32(bounds[0]);
+                        upperBound = Convert.ToInt32(bounds[1]);
+
+
+                        try {               //=> guard against non-ascending range
+                            if (upperBound < lowerBound) {
+                                throw new Exception();
+                                
+                            } else {
+                                isValid = true;
+
+                            }
+
+                        } catch(Exception e){
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine(".. Invaild input, lower bound({0}) is larger than upper bound({1}).\n", lowerBound, upperBound);
+
+                        }
+
+                    } catch(FormatException e) {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine(".. Invaild input, both inputs must be of TYPE integer.\n");
+
+                    }
+
+                } catch (ArgumentOutOfRangeException e) {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(".. Invalid range, range requires two values.\n");
+
+                }
+
+            } while (isValid != true);
+
+            arr = new int[size];                        //=> instantiate the array
+
+            for(int i = 0; i < arr.Length; i++) {       //=> populate the array
+                arr[i] = rnd.Next(lowerBound, upperBound);
+ 
+            }
+            
+            Console.WriteLine("\n\n> Status: Unsorted\n> Timestamp: {0}\n> Type: {1}\n> Size: {2}\n> Range: {3} to {4}\n", DateTime.Now, arr.GetType(), size, lowerBound, upperBound);
+            MergeSort.displayArray(arr, arr.Length);    //=> display the unsorted array
+
+            MergeSort.mergeSort(arr, arr.Length);       //=> sort the array using mergeSort
+
+            Console.WriteLine("\n\n> Status: Sorted\n> Timestamp: {0}\n> Merge Sort (recursive)\n> Type: {1}\n> Size: {2}\n> Range: {3} to {4}\n", DateTime.Now, arr.GetType(), size, lowerBound, upperBound);
+            MergeSort.displayArray(arr, arr.Length);    //=> display the sorted array
+
+        }
+
+    }
+
+}
---
+++ merge_sort/c#/MergeSortCS/MergeSortLibrary/MergeSort.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MergeSortLib {
+
+    public class MergeSort {
+        /// <summary>
+        /// Accepts an unsorted array of integers and the current number of elements in the array then invocates a 
+        /// helper function that performs the recursive implemenation of merge sort. Asymptotic run-time O(nlogn).
+        /// </summary>
+        /// <param name="arr">input array of type integer<./param>
+        /// <param name="numElements">number of elements currently in the array.</param>
+        public static void mergeSort(int[] arr, int numElements) {
+            rMergeSort(arr, 0, (numElements - 1));
+
+        }
+
+        /// <summary>
+        /// Implements head recursion to sub-divide an array until it can no longer be divided anymore (atom) which
+        /// is the base case. The recursion stack then returns and calls Merge to merge the two sub-arrays into a 
+        /// larged combined array. These sub-arrays are a product of the recursive calls and therefore are sorted
+        /// arrays (i.e. an array of one element is a sorted array) which facilitates sorting the larger array.
+        /// </summary>
+        /// <param name="arr">input integer array to be sorted.</param>
+        /// <param name="lowerBound">begining array index of arr.</param>
+        /// <param name="upperBound">last array index of arr.</param>
+        private static void rMergeSort(int[] arr, int lowerBound, int upperBound) {
+            int middle;
+
+            if(lowerBound < upperBound) {                   //=> base case, when array length is size one
+                middle = (upperBound + lowerBound) / 2;
+                rMergeSort(arr, lowerBound, middle);        //=> recursive call split the array first-half
+                rMergeSort(arr, (middle + 1), upperBound);  //=> recursive call split the array second-half
+                Merge(arr, lowerBound, middle, upperBound);
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Merge takes values from an input array and sorts them into a transfer array, the final step is to
+        /// copy the contents of the transfer array (which are sorted) back into the input array at the correct
+        /// indicies.
+        /// </summary>
+        /// <param name="arr">input integer array to be sorted.</param>
+        /// <param name="lowerBound">begining array index of arr.</param>
+        /// <param name="middle">middle index of the array.</param>
+        /// <param name="upperBound">last array index of arr.</param>
+        private static void Merge(int[] arr, int lowerBound, int middle, int upperBound) {
+            int[] xferArr = new int[(upperBound - lowerBound) + 1];
+            int x = 0;                                      //=> transfer array index
+            int lftLowerBound = lowerBound;                 //=> left-side lower bound index
+            int rghtLowerBound = middle + 1;                //=> right-side lower bound index
+            
+            while ((lftLowerBound <= middle) && (rghtLowerBound <= upperBound)) {   //=> both sides have elements
+                if (arr[lftLowerBound] < arr[rghtLowerBound]) {
+                    xferArr[x] = arr[lftLowerBound];
+                    lftLowerBound += 1;
+
+                } else {
+                    xferArr[x] = arr[rghtLowerBound];
+                    rghtLowerBound += 1;
+
+                }
+                x += 1;
+
+            }
+
+            while(lftLowerBound <= middle) {            //=> left-side has elements
+                xferArr[x] = arr[lftLowerBound];
+                lftLowerBound += 1;
+                x += 1;
+
+            }
+
+            while(rghtLowerBound <= upperBound) {       //=> right-side has elements
+                xferArr[x] = arr[rghtLowerBound];
+                rghtLowerBound += 1;
+                x += 1;
+
+            }
+
+            for(int i = 0; i < xferArr.Length; i++) {   //=> transfer elements back into the orginal array
+                arr[lowerBound + i] = xferArr[i];
+
+            }
+
+        }
+
+        /// <summary>
+        /// Writes the array to the console window.
+        /// </summary>
+        /// <param name="arr">input integer array to be displayed.</param>
+        /// <param name="numElements">number of elements in the array.</param>
+        public static void displayArray(int[] arr, int numElements) {
+            for(int i = 0; i < numElements; i++) {
+                if(i != (numElements - 1)) {
+                    Console.Write(arr[i] + ", ");
+
+                } else {
+                    Console.WriteLine(arr[i]);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
